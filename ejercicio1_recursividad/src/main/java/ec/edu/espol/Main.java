eb0b7d1cc09dc3c59703996da18f874250761d3a
leno-mpm <s.pazmino16@gmail.com>
2025-05-14 18:14:56 -0500

    Titulo

--- ejercicio1_recursividad/src/main/java/ec/edu/espol/Main.java
+++ ejercicio1_recursividad/src/main/java/ec/edu/espol/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(".Ejercicio 1: Palíndromo.");
         Scanner sc= new Scanner (System.in);
         System.out.println("Ingresa una palabra: ");
         boolean respuesta= esRecursivo(sc.nextLine());
