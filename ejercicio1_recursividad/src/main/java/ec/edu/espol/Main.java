9d01c69ef2c66964807a96bf5481587beea75d55
gabriel12305 <gpelaezavila@gmail.com>
2025-05-14 18:10:53 -0500

    modify sout

--- ejercicio1_recursividad/src/main/java/ec/edu/espol/Main.java
+++ ejercicio1_recursividad/src/main/java/ec/edu/espol/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Milenaaaaaaaaaaaaaaa");
         Scanner sc= new Scanner (System.in);
         System.out.println("Ingresa una palabra: ");
         boolean respuesta= esRecursivo(sc.nextLine());
@@ -42,5 +42,5 @@ public class Main {
     }
 
     //holaaaaaaaaaaaaaaaa
-    
+
 }
